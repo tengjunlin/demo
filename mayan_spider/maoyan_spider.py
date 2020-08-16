e0d9713402f71dc102afb19da34876112fdda1e4
Grace teng <38376302+tengjunlin@users.noreply.github.com>
2020-08-16 20:46:01 +0800

    Add files via upload

---
+++ mayan_spider/maoyan_spider.py
@@ -0,0 +1,59 @@
+import json
+import re
+import requests
+from requests.exceptions import RequestException
+import time
+
+
+def get_one_page(url):
+    """获取网页源码"""
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/65.0.3325.162 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        return None
+
+
+def parse_one_page(html):
+    """解析网页源码"""
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],  # 去掉'主演：'这三个字符
+            'time': item[4].strip(),  # 去掉'上映时间：'这五个字符
+            'score': item[5] + item[6]
+        }
+
+
+def write_to_file(content):
+    """写入到文本文件"""
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+
+def main(offset):
+    """主函数"""
+    url = 'https://maoyan.com/board/4?offset=' + str(offset)
+    html = get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+
+if __name__ == '__main__':
+    """"调用主函数10次"""
+    for i in range(0, 10):
+        main(offset=i*10)
+        time.sleep(1)
